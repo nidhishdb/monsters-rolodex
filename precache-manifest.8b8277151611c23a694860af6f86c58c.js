3b5d01d46cf7600a88cfc0e247873cee0fbca2ee
hashelse <hashelse@gmail.com>
2019-08-11 22:04:27 +0530

    Updates

---
+++ precache-manifest.8b8277151611c23a694860af6f86c58c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de98e582ca74ef46981bec3e69bf11f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1525752e549304a94656",
+    "url": "/monsters-rolodex/static/css/main.d4ec63a3.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1525752e549304a94656",
+    "url": "/monsters-rolodex/static/js/main.1e83aab8.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
